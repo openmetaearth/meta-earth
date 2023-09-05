aa09761f503d76bc97346ba6ad8490e7b9c45ad7
me-metaearth <me.git.1020@gmail.com>
2023-09-05 17:05:09 +0800

    merge:  new gas distribution schema

--- cmd/me-chaind/cmd/root.go
+++ cmd/me-chaind/cmd/root.go
@@ -70,7 +70,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   version.AppName,
-		Short: "Wasm Daemon (server)",
+		Short: "me-chain Daemon (server)",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
