c2d24a6f451b382f71e87105f6ff2a926a88a207
me-metaearth <me.git.1020@gmail.com>
2023-12-22 14:45:29 +0800

    fix: reset stake for upgrade

--- app/upgrade_v0.0.2.go
+++ app/upgrade_v0.0.2.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -8,35 +9,36 @@ import (
 	"time"
 )
 
-const (
-	UpgradeNameV0_0_2              = "v0.0.2"
-	validatorOperatorAddressNode1  = "me1fm7xpeu2uxp03afq4ea9v27l5krvq29yqnc7nn"
-	validatorOperatorAddressNode2  = "me1fp67zvjqmhl3xc7dstepht7nh46jtzxcalytwk"
-	validatorOperatorAddressNode3  = "me16a9ku63u7766zvrgpuyzpu6fvmmt0mjf57jn2z"
-	validatorOperatorAddressNode4  = "me1cfhxhmtjr6c9h332fdd6enenvr6f6er658508e"
-	validatorOperatorAddressNode5  = "me1vevcep3q6gg5q8rfha2naggmrnclgsapv3grcd"
-	validatorOperatorAddressNode6  = "me1a8uw8uswg3puyr70hj7yuaxp894900dfayqxx7"
-	validatorOperatorAddressNode7  = "me1mmgy7xl9zktyd6ajpxqfajuzyc0x6wpjuwgpkn"
-	validatorOperatorAddressNode8  = "me1wylamlxz8juklcywmfnrjeevkuszzv4r2gd9hd"
-	validatorOperatorAddressNode9  = "me1hx609plt8tyrptuxsntmvhcgfl9rejerr24p06"
-	validatorOperatorAddressNode10 = "me109vphldcx95cl486pguzll8c6teufatn57wgfx"
-	validatorOperatorAddressNode11 = "me1ypmq0dt6y983jf24jpw8u97xm66au0ltfsdlhc"
-	validatorOperatorAddressNode12 = "me1nhkxkc8pn4mstug3fd38xad9swu85wxzzzn7ga"
-	validatorOperatorAddressNode13 = "me18y5rykc4293v50el4yn4fan9gf5qna86evpym5"
-	validatorOperatorAddressNode14 = "me1ldav99eec6nm3vp2n8lhza4j792znqfuhecrlg"
-	validatorOperatorAddressNode15 = "me1k8fypadjt6z8yt4ufpx2v639har40w6245k3xj"
-	validatorOperatorAddressNode16 = "me1k9pdvavle7n6fe7w9seezazlqqs72g835p6qxa"
-	validatorOperatorAddressNode17 = "me1t4atmvxqaffpwxjkpm5lxt83q3ja9y5d8nr64j"
-	validatorOperatorAddressNode18 = "me14m0dhjc6rs084y427ptnjyc4hfefxhzmyedrga"
-	validatorOperatorAddressNode19 = "me1asfkp2ua7splwaf6lqqru3akcrw2l3dupvmewp"
-	validatorOperatorAddressNode20 = "me1685gnusadlv86tl73h935626qnsr8u830msdsc"
-	validatorOperatorAddressNode21 = "me16d4act7my9tshkxulvr75ntdwc0334pxknck05"
-	validatorOperatorAddressNode22 = "me1vlnx284eqf5hlz59y4zdnfx0de5j8zjds5ny4z"
-	validatorOperatorAddressNode23 = "me1kmu7alqc8vlhmy3l4wcw8ephva42asaapt4qn3"
-	validatorOperatorAddressNode24 = "me1z4s5n9uqk6yjwca83vezahvnwp3xn9gusjwyze"
-	validatorOperatorAddressNode25 = "me15lvx9x4e248jjpdxspstg4knwkyew6hhtrcu5j"
-	validatorOperatorAddressNode26 = "me1v8wtd9qk9v08np3dd4khxgqwhvv59zgypzf7g7"
-	validatorOperatorAddressNode27 = "me1ar9pkzk02eu0qksm7pzuhkcvd2stshvgur75aj"
+const UpgradeNameV0_0_2 = "v0.0.2"
+
+var (
+	validatorOperatorAddressNode1  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1fm7xpeu2uxp03afq4ea9v27l5krvq29yqnc7nn")).String()
+	validatorOperatorAddressNode2  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1fp67zvjqmhl3xc7dstepht7nh46jtzxcalytwk")).String()
+	validatorOperatorAddressNode3  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me16a9ku63u7766zvrgpuyzpu6fvmmt0mjf57jn2z")).String()
+	validatorOperatorAddressNode4  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1cfhxhmtjr6c9h332fdd6enenvr6f6er658508e")).String()
+	validatorOperatorAddressNode5  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1vevcep3q6gg5q8rfha2naggmrnclgsapv3grcd")).String()
+	validatorOperatorAddressNode6  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1a8uw8uswg3puyr70hj7yuaxp894900dfayqxx7")).String()
+	validatorOperatorAddressNode7  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1mmgy7xl9zktyd6ajpxqfajuzyc0x6wpjuwgpkn")).String()
+	validatorOperatorAddressNode8  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1wylamlxz8juklcywmfnrjeevkuszzv4r2gd9hd")).String()
+	validatorOperatorAddressNode9  = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1hx609plt8tyrptuxsntmvhcgfl9rejerr24p06")).String()
+	validatorOperatorAddressNode10 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me109vphldcx95cl486pguzll8c6teufatn57wgfx")).String()
+	validatorOperatorAddressNode11 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1ypmq0dt6y983jf24jpw8u97xm66au0ltfsdlhc")).String()
+	validatorOperatorAddressNode12 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1nhkxkc8pn4mstug3fd38xad9swu85wxzzzn7ga")).String()
+	validatorOperatorAddressNode13 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me18y5rykc4293v50el4yn4fan9gf5qna86evpym5")).String()
+	validatorOperatorAddressNode14 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1ldav99eec6nm3vp2n8lhza4j792znqfuhecrlg")).String()
+	validatorOperatorAddressNode15 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1k8fypadjt6z8yt4ufpx2v639har40w6245k3xj")).String()
+	validatorOperatorAddressNode16 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1k9pdvavle7n6fe7w9seezazlqqs72g835p6qxa")).String()
+	validatorOperatorAddressNode17 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1t4atmvxqaffpwxjkpm5lxt83q3ja9y5d8nr64j")).String()
+	validatorOperatorAddressNode18 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me14m0dhjc6rs084y427ptnjyc4hfefxhzmyedrga")).String()
+	validatorOperatorAddressNode19 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1asfkp2ua7splwaf6lqqru3akcrw2l3dupvmewp")).String()
+	validatorOperatorAddressNode20 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1685gnusadlv86tl73h935626qnsr8u830msdsc")).String()
+	validatorOperatorAddressNode21 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me16d4act7my9tshkxulvr75ntdwc0334pxknck05")).String()
+	validatorOperatorAddressNode22 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1vlnx284eqf5hlz59y4zdnfx0de5j8zjds5ny4z")).String()
+	validatorOperatorAddressNode23 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1kmu7alqc8vlhmy3l4wcw8ephva42asaapt4qn3")).String()
+	validatorOperatorAddressNode24 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1z4s5n9uqk6yjwca83vezahvnwp3xn9gusjwyze")).String()
+	validatorOperatorAddressNode25 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me15lvx9x4e248jjpdxspstg4knwkyew6hhtrcu5j")).String()
+	validatorOperatorAddressNode26 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1v8wtd9qk9v08np3dd4khxgqwhvv59zgypzf7g7")).String()
+	validatorOperatorAddressNode27 = sdk.ValAddress(sdk.MustAccAddressFromBech32("me1ar9pkzk02eu0qksm7pzuhkcvd2stshvgur75aj")).String()
 )
 
 var UpgradeV0_0_2 = Upgrade{
@@ -68,66 +70,110 @@ func CreateUpgradeHandlerV0_0_2(
 		}
 
 		ctx.Logger().Info("==> replace validator operator address")
+		adminAddr, err := app.StakingKeeper.GetGlobalAdminAddr(ctx)
+		if err != nil {
+			return newVM, err
+		}
+		ctx.Logger().Info(fmt.Sprintf("adminAddr: %s", adminAddr.String()))
+		stakes := app.StakingKeeper.GetAllStakes(ctx)
+		for _, stake := range stakes {
+			fmt.Println("stake: ", stake.String())
+		}
 		validators := app.StakingKeeper.GetAllValidators(ctx)
 		for _, validator := range validators {
+			stake, found := app.StakingKeeper.GetStake(ctx, adminAddr, validator.GetOperator())
+			if !found {
+				return newVM, fmt.Errorf("not found stake")
+			}
+			ctx.Logger().Info("==>stake", "staker", stake.StakerAddress, "old stake", stake.ValidatorAddress)
+			ctx.Logger().Info("==>validator", "old validator", validator.GetOperator())
+
 			app.StakingKeeper.RemoveValidatorKeys(ctx, validator.GetOperator())
 			app.StakingKeeper.DeleteLastValidatorPower(ctx, validator.GetOperator())
 			app.StakingKeeper.DeleteValidatorByPowerIndex(ctx, validator)
+
 			switch validator.GetMoniker() {
 			case "node1":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode1)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode1
+				stake.ValidatorAddress = validatorOperatorAddressNode1
 			case "node2":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode2)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode2
+				stake.ValidatorAddress = validatorOperatorAddressNode2
 			case "node3":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode3)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode3
+				stake.ValidatorAddress = validatorOperatorAddressNode3
 			case "node4":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode4)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode4
+				stake.ValidatorAddress = validatorOperatorAddressNode4
 			case "node5":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode5)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode5
+				stake.ValidatorAddress = validatorOperatorAddressNode5
 			case "node6":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode6)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode6
+				stake.ValidatorAddress = validatorOperatorAddressNode6
 			case "node7":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode7)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode7
+				stake.ValidatorAddress = validatorOperatorAddressNode7
 			case "node8":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode8)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode8
+				stake.ValidatorAddress = validatorOperatorAddressNode8
 			case "node9":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode9)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode9
+				stake.ValidatorAddress = validatorOperatorAddressNode9
 			case "node10":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode10)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode10
+				stake.ValidatorAddress = validatorOperatorAddressNode10
 			case "node11":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode11)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode11
+				stake.ValidatorAddress = validatorOperatorAddressNode11
 			case "node12":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode12)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode12
+				stake.ValidatorAddress = validatorOperatorAddressNode12
 			case "node13":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode13)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode13
+				stake.ValidatorAddress = validatorOperatorAddressNode13
 			case "node14":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode14)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode14
+				stake.ValidatorAddress = validatorOperatorAddressNode14
 			case "node15":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode15)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode15
+				stake.ValidatorAddress = validatorOperatorAddressNode15
 			case "node16":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode16)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode16
+				stake.ValidatorAddress = validatorOperatorAddressNode16
 			case "node17":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode17)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode17
+				stake.ValidatorAddress = validatorOperatorAddressNode17
 			case "node18":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode18)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode18
+				stake.ValidatorAddress = validatorOperatorAddressNode18
 			case "node19":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode19)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode19
+				stake.ValidatorAddress = validatorOperatorAddressNode19
 			case "node20":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode20)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode20
+				stake.ValidatorAddress = validatorOperatorAddressNode20
 			case "node21":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode21)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode21
+				stake.ValidatorAddress = validatorOperatorAddressNode21
 			case "node22":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode22)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode22
+				stake.ValidatorAddress = validatorOperatorAddressNode22
 			case "node23":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode23)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode23
+				stake.ValidatorAddress = validatorOperatorAddressNode23
 			case "node24":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode24)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode24
+				stake.ValidatorAddress = validatorOperatorAddressNode24
 			case "node25":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode25)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode25
+				stake.ValidatorAddress = validatorOperatorAddressNode25
 			case "node26":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode26)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode26
+				stake.ValidatorAddress = validatorOperatorAddressNode26
 			case "node27":
-				validator.OperatorAddress = sdk.ValAddress(sdk.MustAccAddressFromBech32(validatorOperatorAddressNode27)).String()
+				validator.OperatorAddress = validatorOperatorAddressNode27
+				stake.ValidatorAddress = validatorOperatorAddressNode27
 			}
 
 			devOperatorAddr := app.StakingKeeper.GetDevOperatorAddress(ctx)
@@ -136,8 +182,11 @@ func CreateUpgradeHandlerV0_0_2(
 			if err != nil {
 				return nil, err
 			}
+			ctx.Logger().Info("==> reset validator", "validator", validator.GetOperator().String())
 			app.StakingKeeper.SetValidator(ctx, validator)
 			app.StakingKeeper.SetValidatorByPowerIndex(ctx, validator)
+			ctx.Logger().Info("==> reset stake", "staker", stake.StakerAddress, "validator", stake.ValidatorAddress)
+			app.StakingKeeper.SetStake(ctx, stake)
 			if err := app.StakingKeeper.Hooks().AfterValidatorCreated(ctx, validator.GetOperator()); err != nil {
 				return nil, err
 			}
